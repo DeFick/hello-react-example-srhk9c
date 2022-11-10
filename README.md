8badac546fd5f13763928b5151e65a36d38eced5
DeFick <114681524+DeFick@users.noreply.github.com>
2022-11-10 14:28:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello-react-example-srhk9c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hello-react-example-srhk9c)
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+ReactDOM.render(
+  <h1>kurwa mac</h1>,
+  document.getElementById('root')
+);
